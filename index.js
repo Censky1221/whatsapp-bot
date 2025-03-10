542976eee2d4dbbca4c2fa47622596c9bccf6b46
Censky1221 <storeyoga692@gmail.com>
2025-03-11 04:55:27 +0700

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+
 const { addItemToList } = require("./lib/store");
 
 // Contoh: Menambahkan item ke list grup
